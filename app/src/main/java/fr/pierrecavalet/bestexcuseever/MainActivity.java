32ce488dbce9c8660a61853e8c45f892b4a3da83
Cavalet Pierre <pierre.cavalet@etu.umontpellier.fr>
2015-10-25 17:55:50 +0100

    Base client with socket.io communication

--- app/src/main/java/fr/pierrecavalet/bestexcuseever/MainActivity.java
+++ app/src/main/java/fr/pierrecavalet/bestexcuseever/MainActivity.java
@@ -2,12 +2,82 @@ package fr.pierrecavalet.bestexcuseever;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.github.nkzawa.emitter.Emitter;
+import com.github.nkzawa.socketio.client.IO;
+import com.github.nkzawa.socketio.client.Socket;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URISyntaxException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Socket mSocket;
+    {
+        try {
+            mSocket = IO.socket("http://149.202.49.136:1337");
+        } catch (URISyntaxException e) {}
+    }
+
+    private EditText mInputMessageView;
+    private TextView mOutputMessageView;
+
+    private void attemptSend() {
+        String message = mInputMessageView.getText().toString().trim();
+        if (TextUtils.isEmpty(message)) {
+            return;
+        }
+        mInputMessageView.setText("");
+        mSocket.emit("test", message);
+    }
+
+    private Emitter.Listener onNewMessage = new Emitter.Listener() {
+        @Override
+        public void call(final Object... args) {
+            MainActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mOutputMessageView.setText(mOutputMessageView.getText() + "test ");
+                }
+            });
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mSocket.on("message", onNewMessage);
+        mSocket.connect();
         setContentView(R.layout.activity_main);
+        mInputMessageView = (EditText) findViewById(R.id.input);
+        mOutputMessageView = (TextView) findViewById(R.id.output);
+        Button bouton = (Button) findViewById(R.id.button);
+        bouton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        attemptSend();
+                    }
+                }
+        );
     }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mSocket.disconnect();
+        mSocket.off("message", onNewMessage);
+    }
+
 }
